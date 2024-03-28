10db95ed2121f70420a7e2debda090a6e953a4f0
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-03-28 15:54:11 +0600

    Price Section Page Create

---
+++ note.js
@@ -0,0 +1,43 @@
+/**
+Single price with details
+SinglePriceWithDetails
+
+Three tiers
+ThreeTiers
+
+Three tiers with dividers
+ThreeTiersWithDividers
+
+Three tiers with emphasized tier
+ThreeTiersWithEmphasizedTier
+
+Three tiers with toggle
+ThreeTiersWithToggle
+
+Three tiers with toggle on dark
+ThreeTiersWithToggleOnDark
+
+Four tiers with toggle
+FourTiersWithToggle
+
+Two tiers with extra tier
+TwoTiersWithExtraTier
+
+With comparison table
+WithComparisonTable
+
+
+With comparison table on dark
+WithComparisonTableOnDark
+
+Three tiers with feature comparison
+ThreeTiersWithFeatureComparison
+
+Two tiers
+TwoTiers
+
+Two tiers with emphasized tier
+TwoTiersWithEmphasizedTier
+
+
+ */
--- src/pages/marketing/pageSection/Price/index.jsx
+++ src/pages/marketing/pageSection/Price/index.jsx
@@ -1,5 +1,39 @@
+import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import FourTiersWithToggle from "./FourTiersWithToggle";
+import SinglePriceWithDetails from "./SinglePriceWithDetails";
+import ThreeTiers from "./ThreeTiers";
+import ThreeTiersWithDividers from "./ThreeTiersWithDividers";
+import ThreeTiersWithEmphasizedTier from "./ThreeTiersWithEmphasizedTier";
+import ThreeTiersWithFeatureComparison from "./ThreeTiersWithFeatureComparison";
+import ThreeTiersWithToggle from "./ThreeTiersWithToggle";
+import ThreeTiersWithToggleOnDark from "./ThreeTiersWithToggleOnDark";
+import TwoTiers from "./TwoTiers";
+import TwoTiersWithEmphasizedTier from "./TwoTiersWithEmphasizedTier";
+import TwoTiersWithExtraTier from "./TwoTiersWithExtraTier";
+import WithComparisonTable from "./WithComparisonTable";
+import WithComparisonTableOnDark from "./WithComparisonTableOnDark";
+
 const Price = () => {
-  return <div>price</div>;
+  return (
+    <ComponentPageContainer
+      pageTitle={"Pricing Sections"}
+      breadcrumb={"Marketing / Page Sections"}
+    >
+      <SinglePriceWithDetails />
+      <ThreeTiers />
+      <ThreeTiersWithDividers />
+      <ThreeTiersWithEmphasizedTier />
+      <ThreeTiersWithToggle />
+      <ThreeTiersWithToggleOnDark />
+      <FourTiersWithToggle />
+      <TwoTiersWithExtraTier />
+      <WithComparisonTable />
+      <WithComparisonTableOnDark />
+      <ThreeTiersWithFeatureComparison />
+      <TwoTiers />
+      <TwoTiersWithEmphasizedTier />
+    </ComponentPageContainer>
+  );
 };
 
 export default Price;
