0ed14f99ba620403a8da82a77ce4b8f42b86b07b
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-03-28 14:16:41 +0600

    CTA Section Created

--- src/pages/marketing/pageSection/Cta/index.jsx
+++ src/pages/marketing/pageSection/Cta/index.jsx
@@ -1,5 +1,35 @@
+import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import CenteredOnDarkPanel from "./CenteredOnDarkPanel";
+import DarkPanelWithAppScreenshot from "./DarkPanelWithAppScreenshot";
+import SimpleCentered from "./SimpleCentered";
+import SimpleCenteredOnBrand from "./SimpleCenteredOnBrand";
+import SimpleCenteredOnDark from "./SimpleCenteredOnDark";
+import SimpleJustified from "./SimpleJustified";
+import SimpleJustifiedOnLightBrand from "./SimpleJustifiedOnLightBrand";
+import SimpleStacked from "./SimpleStacked";
+import SplitWithImage from "./SplitWithImage";
+import TwoColumnsWithPhotoOnDark from "./TwoColumnsWithPhotoOnDark";
+import WithImageTiles from "./WithImageTiles";
+
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <ComponentPageContainer
+      pageTitle={"CTA Sections"}
+      breadcrumb={"Marketing / Page Sections"}
+    >
+      <DarkPanelWithAppScreenshot />
+      <SimpleStacked />
+      <CenteredOnDarkPanel />
+      <SimpleCentered />
+      <SimpleCenteredOnDark />
+      <SimpleCenteredOnBrand />
+      <SimpleJustified />
+      <SimpleJustifiedOnLightBrand />
+      <SplitWithImage />
+      <TwoColumnsWithPhotoOnDark />
+      <WithImageTiles />
+    </ComponentPageContainer>
+  );
 };
 
 export default Cta;
