475e69328a7e30dc8f3d23a65efd85ba01f47538
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-04-19 20:11:31 +0600

    Application UI/Data Display (State Page create) complete

--- src/pages/applications/dataDisplay/Stats/SimpleInCards/index.jsx
+++ src/pages/applications/dataDisplay/Stats/SimpleInCards/index.jsx
@@ -1,7 +1,34 @@
 import ComponentContainer from "../../../../../shared/components/ComponentContainer";
-
+const stats = [
+  { name: "Total Subscribers", stat: "71,897" },
+  { name: "Avg. Open Rate", stat: "58.16%" },
+  { name: "Avg. Click Rate", stat: "24.57%" },
+];
 const SimpleInCards = () => {
-  return <ComponentContainer title={"Simple in cards"}></ComponentContainer>;
+  return (
+    <ComponentContainer title={"Simple in cards"}>
+      <div className="rounded-xl border border-slate-200 p-10 bg-gray-100">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          Last 30 days
+        </h3>
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {stats.map((item) => (
+            <div
+              key={item.name}
+              className="overflow-hidden rounded-lg bg-white px-4 py-5 shadow sm:p-6"
+            >
+              <dt className="truncate text-sm font-medium text-gray-500">
+                {item.name}
+              </dt>
+              <dd className="mt-1 text-3xl font-semibold tracking-tight text-gray-900">
+                {item.stat}
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </ComponentContainer>
+  );
 };
 
 export default SimpleInCards;
