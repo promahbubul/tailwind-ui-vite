8c1c37b8712b9b8f1b5d0ae1f05136bbb95c0685
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-06-02 20:23:08 +0600

    Application UI /  Navigation pages [Tabs] component created

--- src/pages/applications/navigation/Tabs/index.jsx
+++ src/pages/applications/navigation/Tabs/index.jsx
@@ -1,11 +1,30 @@
 import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import BarWithUnderline from "./BarWithUnderline";
+import FullWidthTabsWithUnderline from "./FullWidthTabsWithUnderline";
+import SimpleOnDark from "./SimpleOnDark";
+import TabsInPills from "./TabsInPills";
+import TabsInPillsOnGray from "./TabsInPillsOnGray";
+import TabsInPillsWithBrandColor from "./TabsInPillsWithBrandColor";
+import TabsWithUnderline from "./TabsWithUnderline";
+import TabsWithUnderlineAndBadges from "./TabsWithUnderlineAndBadges";
+import TabsWithUnderlineAndIcons from "./TabsWithUnderlineAndIcons";
 
 const Tabs = () => {
   return (
     <ComponentPageContainer
       pageTitle={"Tabs"}
       breadcrumb={"Application UI / Navigation"}
-    ></ComponentPageContainer>
+    >
+      <TabsWithUnderline />
+      <TabsWithUnderlineAndIcons />
+      <TabsInPills />
+      <TabsInPillsOnGray />
+      <TabsInPillsWithBrandColor />
+      <FullWidthTabsWithUnderline />
+      <BarWithUnderline />
+      <TabsWithUnderlineAndBadges />
+      <SimpleOnDark />
+    </ComponentPageContainer>
   );
 };
 
