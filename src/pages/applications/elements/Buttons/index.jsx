6a1e1e55bec06485f823f320197eefa1d268b4f6
MAHBUBUL ALAM <33549330+promahbubul@users.noreply.github.com>
2024-06-09 14:49:39 +0600

    Application Ui / Elements page Buttons components update

--- src/pages/applications/elements/Buttons/index.jsx
+++ src/pages/applications/elements/Buttons/index.jsx
@@ -1,4 +1,14 @@
 import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import ButtonsWithLeadingIcon from "./ButtonsWithLeadingIcon";
+import ButtonsWithTrailingIcon from "./ButtonsWithTrailingIcon";
+import CircularButtons from "./CircularButtons";
+import PrimaryButtons from "./PrimaryButtons";
+import PrimaryButtonsOnDark from "./PrimaryButtonsOnDark";
+import RoundedPrimaryButtons from "./RoundedPrimaryButtons";
+import RoundedSecondaryButtons from "./RoundedSecondaryButtons";
+import SecondaryButtons from "./SecondaryButtons";
+import SecondaryButtonsOnDark from "./SecondaryButtonsOnDark";
+import SoftButtons from "./SoftButtons";
 
 const Buttons = () => {
   return (
@@ -6,7 +16,16 @@ const Buttons = () => {
       pageTitle={"Buttons"}
       breadcrumb={"Application UI / Elements"}
     >
-      
+      <PrimaryButtons />
+      <SecondaryButtons />
+      <PrimaryButtonsOnDark />
+      <SecondaryButtonsOnDark />
+      <SoftButtons />
+      <ButtonsWithLeadingIcon />
+      <ButtonsWithTrailingIcon />
+      <RoundedPrimaryButtons />
+      <RoundedSecondaryButtons />
+      <CircularButtons />
     </ComponentPageContainer>
   );
 };
