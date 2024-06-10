e81ffeddae2a727cc73ff765a843027cfdcca55b
promahbubul <mahbublalam500@gmail.com>
2024-06-10 12:46:11 +0600

    Application Ui / Page Examples page Settings Screens components update

--- src/pages/applications/pages/SettingsScreens/index.jsx
+++ src/pages/applications/pages/SettingsScreens/index.jsx
@@ -1,7 +1,12 @@
 import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import SidebarOnDark from "./SidebarOnDark";
+import Stacked from "./Stacked";
 
 const SettingsScreen = () => {
-  return <ComponentPageContainer pageTitle={"Tailwind CSS Settings Screens"} breadcrumb={"Application UI Page / Examples"}></ComponentPageContainer>
+  return <ComponentPageContainer pageTitle={"Tailwind CSS Settings Screens"} breadcrumb={"Application UI Page / Examples"}>
+    <SidebarOnDark />
+    <Stacked />
+  </ComponentPageContainer>
 };
 
 export default SettingsScreen;
