34c929b3ee07ce1558eae447ec06eaee38127648
promahbubul <mahbublalam500@gmail.com>
2024-06-10 20:11:12 +0600

    Ecommerce / Components page Order Summaries components created

--- src/pages/ecommerce/components/OrderSummaries/index.jsx
+++ src/pages/ecommerce/components/OrderSummaries/index.jsx
@@ -1,5 +1,21 @@
+import ComponentPageContainer from "../../../../shared/components/ComponentPageContainer";
+import SimpleWithFullOrderDetails from "./SimpleWithFullOrderDetails";
+import WithLargeImagesAndProgressBars from "./WithLargeImagesAndProgressBars";
+import WithProgressBars from "./WithProgressBars";
+import WithSplitImage from "./WithSplitImage";
+
 const OrderSummaries = () => {
-  return <div>OrderSummaries</div>;
+  return (
+    <ComponentPageContainer
+      pageTitle={"Tailwind CSS Order Summaries"}
+      breadcrumb={"Ecommerce / Components"}
+    >
+      <WithSplitImage />
+      <WithProgressBars />
+      <WithLargeImagesAndProgressBars />
+      <SimpleWithFullOrderDetails />
+    </ComponentPageContainer>
+  );
 };
 
 export default OrderSummaries;
